a65483a6b2137eae135784d7df70db181efc0412
Alex3132 <alexis.zueras.pro@gmail.com>
2021-08-12 13:42:14 +0200

    tutorial finished 1.1

--- website/views.py
+++ website/views.py
@@ -1,10 +1,13 @@
+from sqlalchemy.orm import session
+from sqlalchemy.sql.expression import delete
 from sqlalchemy.sql.functions import user
 from website.models import Note
-from flask import Blueprint,render_template, request, flash
+from flask import Blueprint,render_template, request, flash, jsonify
 from flask.helpers import flash
 from flask_login import login_required,current_user
 from .models import Note
 from . import db
+import json
 
 
 views = Blueprint('views',__name__)
@@ -25,3 +28,14 @@ def home():
     return render_template("home.html", user = current_user)
 
 
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
