8914aa6a6de9cec07b30b2b2bf2a13970367d601
Alex3132 <alexis.zueras.pro@gmail.com>
2021-08-12 10:27:16 +0200

    flask login module 2

--- website/auth.py
+++ website/auth.py
@@ -5,7 +5,7 @@ from flask import Blueprint,render_template,request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
-from flask_login import login_user, login_required, logout_user,current_user
+from flask_login import login_user, login_required, logout_user, current_user
 
 
 auth = Blueprint('auth', __name__)
@@ -58,7 +58,7 @@ def sign_up():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
-            logout_user(user,remember=True)
+            logout_user(user, remember=True)
             flash("Account Created", category='success')
             return redirect(url_for('views.home'))
     return render_template("sign_up.html")
